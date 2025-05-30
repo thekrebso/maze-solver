373241332fe38738d21ff725c48c63fbd3b0db63
krebso <krebso@krebso.pl>
2025-05-30 02:34:56 +0200

    added drawing Line onto window's canvas

--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from line import Line
 
 
 class Window:
@@ -21,3 +22,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
