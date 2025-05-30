42a683c927669d3e8654416e04a0b5e56fc8c6ec
krebso <krebso@krebso.pl>
2025-05-30 23:10:04 +0200

    added initial unit test for maze class

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+from point import Point
+from maze import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(Point(0, 0), num_rows, num_cols, 10, 10)
+
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
