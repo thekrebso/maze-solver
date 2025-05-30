2de17f845eab198de9fe3c89a4c47d0b37c941fb
krebso <krebso@krebso.pl>
2025-05-30 02:14:09 +0200

    make private

--- window.py
+++ window.py
@@ -5,9 +5,9 @@ class Window:
     def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title("Maze solver")
-        self.canvas = Canvas()
-        self.canvas.pack(in_=self.__root)
-        self.running = False
+        self.__canvas = Canvas()
+        self.__canvas.pack(in_=self.__root)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -15,9 +15,9 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
